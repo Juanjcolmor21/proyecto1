cd526d3b234b08066c0ee8d67b49060c25ef0f83
Juanjcolmor21 <juanj.colmor@educa.jcyl.es>
2023-04-11 19:51:10 +0200

    segundo commit

--- EclipseGit1/src/Proyecto1/Principal1.java
+++ EclipseGit1/src/Proyecto1/Principal1.java
@@ -3,5 +3,7 @@ package Proyecto1;
 public class Principal1 {
 	public static void main(String[] args) {
 		System.out.println("Hola Git");
+		System.out.println("Hola Git");
+		System.out.println("Hola Git");
 	}
 }
